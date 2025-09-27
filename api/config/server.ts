77e2ade090a21a7d57596692946821aa2ea228f3
James Villarrubia <james.villarrubia@gmail.com>
2025-09-27 15:09:16 -0400

    fix(server): add PUBLIC_URL configuration for Strapi Cloud admin panel asset loading

--- api/config/server.ts
+++ api/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://striking-ball-b079f8c4b0.strapiapp.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
